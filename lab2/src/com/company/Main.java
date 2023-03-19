5f3cbe2dc9740a620b8e9cd3d9c37c44562ffb45
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-03-19 13:00:18 +0100

    lab2 finished

--- lab2/src/com/company/Main.java
+++ lab2/src/com/company/Main.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-//        styczne();
+//        sieczne();
 //        bisekcja();
 //        falsy();
+        styczne();
     }
 
-    public static void styczne(){
+    public static void sieczne(){
         Scanner scanner = new Scanner(System.in);
         int maxIteracje = 100;
 
@@ -131,11 +132,80 @@ public class Main {
         }
     }
 
+    public static void styczne(){//newton
+        Scanner scanner = new Scanner(System.in);
+        int maxIteracje = 100;
+
+        System.out.println("Podaj stopien wielomianu");
+        int n = scanner.nextInt();
+
+        double wspolczynniki[] = new double[n+1];
+        for(int i = wspolczynniki.length-1; i >=0; i--){
+            System.out.print("x^" + (i)+ ": ");
+            wspolczynniki[i] = scanner.nextDouble();
+        }
+
+        System.out.println("Podaj a");
+        double a = scanner.nextDouble();
+
+        System.out.println("Podaj b");
+        double b = scanner.nextDouble();
+
+        System.out.println("Podaj dokladnosc");
+        double epsilon = scanner.nextDouble();
+
+        double x0 = a;
+        double x1 = b;
+
+        double fx0 = wartoscFunkcji(wspolczynniki,x0);
+        double fx1 = wartoscFunkcji(wspolczynniki,x1);
+        double x2 = 0;
+
+
+        int iteracje = 0;
+        while (iteracje < maxIteracje){
+            double dfdx = (fx1 - fx0) / (x1 - x0);
+            x2 = x1 - fx1 / dfdx;
+            double fx2 = wartoscFunkcji(wspolczynniki, x2);
+
+            if (Math.abs(fx2) < epsilon) {
+                System.out.println(x2);
+                return;
+            }
+
+            x0 = x1;
+            fx0 = fx1;
+            x1 = x2;
+            fx1 = fx2;
+            iteracje++;
+        }
+    }
+
     public static double wartoscFunkcji(double wspolczynniki[], double x){
         double value = 0;
         for(int i = wspolczynniki.length-1; i >=0; i--){
             value+=Math.pow(x,i)*wspolczynniki[i];
         }
         return value;
+    }
+
+    public static double obliczWartoscPochodnej(double[] wspolczynniki, double x) {
+        double value = 0;
+        for (int i = 0; i < wspolczynniki.length-1; i++) {
+            value += (wspolczynniki.length-1 - i) * wspolczynniki[i] * Math.pow(x, wspolczynniki.length-1 - i - 1);
+        }
+        return value;
+    }
+
+
+    //Przekazuj wspolczynniki w odwrotnej kolejności - tzn 3x^2 + x + 2 ==> {2,1,3}
+    public static double obliczWartosc2giejPochodnej(double[] wspolczynniki, double x) {
+        double result = 0.0;
+        int n = wspolczynniki.length - 1; // stopień wielomianu
+        for (int i = 2; i <= n; i++) {
+            result += i * (i - 1) * wspolczynniki[i] * Math.pow(x, i - 2);
+        }
+
+        return result;
     }
 }
