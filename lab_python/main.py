939c16c8f9a098824260660815c8c74a09c50cef
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-05-08 11:01:45 +0200

    wip2

--- lab_python/main.py
+++ lab_python/main.py
@@ -1,11 +1,13 @@
 from numdifftools import Derivative
 
+
 def f(x):
     return x * x
 
 
 a = -1
 b = 1
+
 
 def metoda_trapezow_prosta():
     fa = f(a)
@@ -26,49 +28,56 @@ def metoda_simpsona_prosta():
     df4 = Derivative(f, n=4)
     fa = f(a)
     fb = f(b)
-    result= ((b - a) / 6) * (fa + 4 * f((a + b) / 2) + fb)
+    result = ((b - a) / 6) * (fa + 4 * f((a + b) / 2) + fb)
     print("Metoda simpsona prosta")
     print(result)
 
 
 def metoda_trapezow_zlozona():
-    n = 5  # - ilość podprzedziałów
-    h = (b-a) / n  # - szerokość podprzedziału
+    n = 4  # - ilość podprzedziałów
+    h = (b - a) / n  # - szerokość podprzedziału
     suma = 0
-    x = a+h
+    x = a + h
 
-    i = 0
+    i = 1
     while i < n:
         suma += f(x)
-        x+=h
+        x += h
         i += 1
-    suma = h * (0.5 * f(a) + suma + 0.5 * f(b))
+
+    suma = h * (0.5 * f(a) + 0.5 * f(b) + suma)
     df2 = Derivative(f, n=2)
-    R=(-((b-a)*h**2)/12)*df2((a+b)/2)
+    R = (-((b - a) * h ** 2) / 12) * df2((a + b) / 2)
     print("Metoda trapezów złożona")
     print(suma)
     print(R)
 
+
 def metoda_simpsona_zlozona():
-    n = 10  # - ilość podprzedziałów
+    n = 4  # - ilość podprzedziałów
+    if n % 2 == 1:
+        print("n musi być parzyste")
+        return
+
     h = (b - a) / n
-    S = 0
+    suma = 0
     i = 0
-    while i <= n:
+
+    while i < n:
         x = a + i * h
-        if i == 0 or i == n:
-            S += f(x)
-        elif i % 2 == 1:
-            S += 4 * f(x)
+        if i % 2 == 0:
+            suma += 2 * f(x)
         else:
-            S += 2 * f(x)
+            suma += 4 * f(x)
         i += 1
-    S *= h / 3
+    suma *= h / 3
+
     print("metoda simpsona złożona")
-    print(S)
+    print(suma)
+
 
 # metoda_simpsona_prosta()
 # metoda_trapezow_prosta()
-metoda_trapezow_zlozona()
-# metoda_simpsona_zlozona()
+# metoda_trapezow_zlozona()
+metoda_simpsona_zlozona()
 # interpolacja i aproksymacja(sredniokwadratowa)
