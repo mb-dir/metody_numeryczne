51eaa96fc6b047d7ab9dcb9f0673735506808d57
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-05-21 22:37:58 +0200

    Rewrite to sympy

--- lab_python/main.py
+++ lab_python/main.py
@@ -1,60 +1,77 @@
-from numdifftools import Derivative
-# NAPRAWA TABELI PRZESTAWNEJ
-# -zliczamy wiersze i kolumny(ILE.NIEPUSTYCH)
-# =PRZESUNIĘCIE($A$8(początek wierszy);ZAOKR.DO.CAŁK((WIERSZ()-WIERSZ($A$19(pierwszy wiersz do wypełniania)))/$H$9(ilość kolumn));0)
-# =PRZESUNIĘCIE($B$7(początek kolumn);0;MOD((WIERSZ()-WIERSZ($B$19(pierwszy wiersz do wypełniania)));$H$9(ilość kolumn)))
-# =WYSZUKAJ.PIONOWO(A19(pierwszy wiersz do wypełniania);$A$7:$E$13(zakres całej tabeli); PODAJ.POZYCJĘ(B19(pierwszy wiersz do wypełniania);$B$7:$E$7(zakres nagłówków kolumn);0)+1;0)
-# SourceData:=Range("A4:L200").SpecialCells(xlCellTypeConstants)
+from sympy import Symbol, diff
+
+
 def f(x):
-    return x * x
+    return x ** 2
 
 
 a = -1
 b = 1
+
+# Tworzenie symbolicznej zmiennej x
+x = Symbol('x')
+
+# Tworzenie symbolicznej funkcji f(x)
+f_expr = x ** 2
 
 
 def metoda_trapezow_prosta():
     fa = f(a)
     fb = f(b)
 
-    df2 = Derivative(f, n=2)
+    # Obliczanie 2-tej pochodnej
+    df2_expr = diff(f_expr, x, 2)
+    # Obliczanie wartości 2-tej pochodnej w punkcie b
+    df2_value = df2_expr.subs(x, b)
 
     result = ((b - a) / 2) * (fa + fb)
-    blad = (((b - a) ** 3) * df2(b)) / 12
+    blad = (((b - a) ** 3) * df2_value) / 12
+
     print("Metoda trapezów prosta")
+    print("Wynik:")
     print(result)
+    print("Błąd:")
     print(blad)
+    print("\n")
 
 
 # metoda simpsona prosta
-
 def metoda_simpsona_prosta():
-    df4 = Derivative(f, n=4)
     fa = f(a)
     fb = f(b)
     result = ((b - a) / 6) * (fa + 4 * f((a + b) / 2) + fb)
     print("Metoda simpsona prosta")
+    print("Wynik:")
     print(result)
+    print("\n")
 
 
 def metoda_trapezow_zlozona():
     n = 4  # - ilość podprzedziałów
     h = (b - a) / n  # - szerokość podprzedziału
     suma = 0
-    x = a + h
+    x1 = a + h
 
     i = 1
     while i < n:
-        suma += f(x)
-        x += h
+        suma += f(x1)
+        x1 += h
         i += 1
 
     suma = h * (0.5 * f(a) + 0.5 * f(b) + suma)
-    df2 = Derivative(f, n=2)
-    R = (-((b - a) * h ** 2) / 12) * df2((a + b) / 2)
+
+    # Obliczanie 2-tej pochodnej
+    df2_expr = diff(f_expr, x, 2)
+    # Obliczanie wartości 2-tej pochodnej w punkcie (a+b)/2
+    df2_value = df2_expr.subs(x, (a + b) / 2)
+
+    R = (-((b - a) * h ** 2) / 12) * df2_value
     print("Metoda trapezów złożona")
+    print("Wynik:")
     print(suma)
+    print("Błąd:")
     print(R)
+    print("\n")
 
 
 def metoda_simpsona_zlozona():
@@ -68,25 +85,31 @@ def metoda_simpsona_zlozona():
     i = 0
 
     while i < n:
-        x = a + i * h
+        x1 = a + i * h
         if i % 2 == 0:
-            suma += 2 * f(x)
+            suma += 2 * f(x1)
         else:
-            suma += 4 * f(x)
+            suma += 4 * f(x1)
         i += 1
     suma *= h / 3
 
-    df4 = Derivative(f, n=4)
-    R=(-((b-a)*h**4)/180)*df4((a+b))
-    # sympy, bo ta biblioteka to zamiast 0 daje przybliżenie zera
+    # Obliczanie 4-tej pochodnej
+    df4_expr = diff(f_expr, x, 4)
+    # Obliczanie wartości 4-tej pochodnej w punkcie (a+b)
+    df4_value = df4_expr.subs(x, a + b)
+
+    R = (-((b - a) * h ** 4) / 180) * df4_value
 
     print("metoda simpsona złożona")
+    print("Wynik:")
     print(suma)
+    print("Błąd:")
     print(R)
+    print("\n")
 
 
-# metoda_simpsona_prosta()
-# metoda_trapezow_prosta()
-# metoda_trapezow_zlozona()
+metoda_trapezow_prosta()
+metoda_simpsona_prosta()
+metoda_trapezow_zlozona()
 metoda_simpsona_zlozona()
 # interpolacja i aproksymacja(sredniokwadratowa)
